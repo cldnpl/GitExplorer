03c4ed75c2e1693a98cd59db51894acd58042192
Claudia Napolitano <120132654+cldnpl@users.noreply.github.com>
2026-07-18 11:26:16 +0200

    Initial Commit

---
+++ gitexplorer/gitexplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitexplorerApp.swift
+//  gitexplorer
+//
+//  Created by Claudia Napolitano on 18.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct gitexplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ gitexplorerTests/gitexplorerTests.swift
@@ -0,0 +1,17 @@
+//
+//  gitexplorerTests.swift
+//  gitexplorerTests
+//
+//  Created by Claudia Napolitano on 18.07.2026.
+//
+
+import Testing
+@testable import gitexplorer
+
+struct gitexplorerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
